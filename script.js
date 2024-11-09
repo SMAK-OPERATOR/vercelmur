786a8e3327d2b437b49f47aa42e7be3b30c92a05
SMAK-OPERATOR <andrey--01@mail.ru>
2024-11-09 14:42:55 +0300

    1

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// функция вызова окна
+function show_popap(id_popap) {
+  var id = "#" + id_popap;
+  $(id).addClass('active');
+}
+ 
+// функция закрытия окна 
+$(".close_popap").click( function(){
+  $(".overlay").removeClass("active");
+  $(".overlay2").removeClass("active");
+});
